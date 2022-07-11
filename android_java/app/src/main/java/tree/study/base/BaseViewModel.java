2f52047afb8af922d6a32426d3894f94789cf7c8
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-07-11 10:24:44 +0900

    android_java > base > update to use viewBinding, dataBinding, LifeCycle (ViewModel, LiveData)

---
+++ android_java/app/src/main/java/tree/study/base/BaseViewModel.java
@@ -0,0 +1,6 @@
+package tree.study.base;
+
+import androidx.lifecycle.ViewModel;
+
+public class BaseViewModel extends ViewModel {
+}
--- android_java/app/src/main/java/tree/study/branch_act/MainActivity.java
+++ android_java/app/src/main/java/tree/study/branch_act/MainActivity.java
@@ -2,14 +2,16 @@ package tree.study.branch_act;
 
 import android.os.Bundle;
 
-import tree.study.R;
 import tree.study.base.BaseActivity;
+import tree.study.databinding.ActivityMainBinding;
 
 public class MainActivity extends BaseActivity {
+	private ActivityMainBinding bind;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		bind = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(bind.getRoot());
 	}
 }
