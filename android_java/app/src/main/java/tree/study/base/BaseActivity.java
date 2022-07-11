8648a6b9d2262c1f53c56b9fa5471999e50157d5
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-07-11 10:11:36 +0900

    android_java > base > create BaseActivity, MainActivity & change basic Theme name

---
+++ android_java/app/src/main/java/tree/study/base/BaseActivity.java
@@ -0,0 +1,6 @@
+package tree.study.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+}
---
+++ android_java/app/src/main/java/tree/study/branch_act/MainActivity.java
@@ -0,0 +1,15 @@
+package tree.study.branch_act;
+
+import android.os.Bundle;
+
+import tree.study.R;
+import tree.study.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
