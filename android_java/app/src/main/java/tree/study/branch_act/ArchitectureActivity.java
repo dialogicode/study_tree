103c0922f1e39ea482382d93d2fcf967bd827621
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-07-12 23:45:07 +0900

    android_java > arch > count page + dataBinding - observe()

--- android_java/app/src/main/java/tree/study/branch_act/ArchitectureActivity.java
+++ android_java/app/src/main/java/tree/study/branch_act/ArchitectureActivity.java
@@ -1,11 +1,11 @@
 package tree.study.branch_act;
 
 import android.os.Bundle;
-import android.view.View;
 
-import androidx.lifecycle.Observer;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import tree.study.R;
 import tree.study.base.BaseActivity;
 import tree.study.branch_vm.ArchitectureViewModel;
 import tree.study.databinding.ActivityArchitectureBinding;
@@ -18,25 +18,8 @@ public class ArchitectureActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		vm = new ViewModelProvider(this).get(ArchitectureViewModel.class);
-		bind = ActivityArchitectureBinding.inflate(getLayoutInflater());
-		setContentView(bind.getRoot());
-
-		bind.plus.setOnClickListener(this::click_plus);
-		bind.minus.setOnClickListener(this::click_minus);
-
-		vm.count.observe(this, new Observer<Integer>() {
-			@Override
-			public void onChanged(Integer integer) {
-				bind.show.setText(integer + "");
-			}
-		});
-	}
-
-	private void click_plus(View view) {
-		vm.increase();
-	}
-
-	private void click_minus(View view) {
-		vm.decrease();
+		bind = DataBindingUtil.setContentView(this, R.layout.activity_architecture);
+		bind.setLifecycleOwner(this);
+		bind.setVm(vm);
 	}
 }
