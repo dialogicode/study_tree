fdcef35143248f8338025d5d2f8f0aa38c27f833
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-07-12 23:11:52 +0900

    android_java > arch > count page + viewBinding

--- android_java/app/src/main/java/tree/study/branch_act/ArchitectureActivity.java
+++ android_java/app/src/main/java/tree/study/branch_act/ArchitectureActivity.java
@@ -1,17 +1,33 @@
 package tree.study.branch_act;
 
 import android.os.Bundle;
+import android.view.View;
 
 import tree.study.base.BaseActivity;
 import tree.study.databinding.ActivityArchitectureBinding;
 
 public class ArchitectureActivity extends BaseActivity {
 	private ActivityArchitectureBinding bind;
+	private int count;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		bind = ActivityArchitectureBinding.inflate(getLayoutInflater());
 		setContentView(bind.getRoot());
+
+		bind.show.setText(count + "");
+		bind.plus.setOnClickListener(this::click_plus);
+		bind.minus.setOnClickListener(this::click_minus);
+	}
+
+	private void click_plus(View view) {
+		count++;
+		bind.show.setText(count + "");
+	}
+
+	private void click_minus(View view) {
+		count--;
+		bind.show.setText(count + "");
 	}
 }
