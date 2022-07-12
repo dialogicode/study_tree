d6eb4c43b214cce83667f82e7acdbb9c8c4014cc
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-07-13 02:01:12 +0900

    android_java > loc > tracking page + layout + LiveData.observe()

--- android_java/app/src/main/java/tree/study/branch_act/LocationActivity.java
+++ android_java/app/src/main/java/tree/study/branch_act/LocationActivity.java
@@ -2,16 +2,33 @@ package tree.study.branch_act;
 
 import android.os.Bundle;
 
+import androidx.lifecycle.ViewModelProvider;
+
 import tree.study.base.BaseActivity;
+import tree.study.branch_vm.LocationViewModel;
 import tree.study.databinding.ActivityLocationBinding;
 
 public class LocationActivity extends BaseActivity {
+	private LocationViewModel vm;
 	private ActivityLocationBinding bind;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		vm = new ViewModelProvider(this).get(LocationViewModel.class);
 		bind = ActivityLocationBinding.inflate(getLayoutInflater());
 		setContentView(bind.getRoot());
+
+		vm.getLatitude().observe(this, doubleV -> bind.latitude.setText(doubleV + ""));
+		vm.getLongitude().observe(this, doubleV -> bind.longitude.setText(doubleV + ""));
+		vm.getAltitude().observe(this, doubleV -> bind.altitude.setText(doubleV + ""));
+		vm.getAccuracy().observe(this, floatV -> bind.accuracy.setText(floatV + ""));
+		vm.getSpeed().observe(this, floatV -> bind.speed.setText(floatV + ""));
+		vm.getAddress().observe(this, addr -> bind.address.setText(addr));
+		vm.getUpdates().observe(this, bool -> bind.updateTv.setText(bool ? "ON" : "OFF"));
+		vm.getSensor().observe(this, bool -> bind.sensorTv.setText(bool ? "GPS Sensor" : "Cell Tower + WIFI"));
+
+		bind.updatesSw.setOnCheckedChangeListener((view, bool) -> vm.getUpdates().setValue(bool));
+		bind.sensorSw.setOnCheckedChangeListener((view, bool) -> vm.getSensor().setValue(bool));
 	}
 }
---
+++ android_java/app/src/main/java/tree/study/branch_vm/LocationViewModel.java
@@ -0,0 +1,83 @@
+package tree.study.branch_vm;
+
+import androidx.lifecycle.MutableLiveData;
+
+import tree.study.base.BaseViewModel;
+
+public class LocationViewModel extends BaseViewModel {
+	private MutableLiveData<Double> latitude;
+	private MutableLiveData<Double> longitude;
+	private MutableLiveData<Double> altitude;
+	private MutableLiveData<Float> accuracy;
+	private MutableLiveData<Float> speed;
+	private MutableLiveData<String> address;
+	private MutableLiveData<Boolean> updates;
+	private MutableLiveData<Boolean> sensor;
+
+	public LocationViewModel() {
+		getLatitude().setValue(0.0);
+		getLongitude().setValue(0.0);
+		getAltitude().setValue(0.0);
+		getAccuracy().setValue(0.0F);
+		getSpeed().setValue(0.0F);
+		getAddress().setValue("");
+		getUpdates().setValue(false);
+		getSensor().setValue(false);
+	}
+
+	public MutableLiveData<Double> getLatitude() {
+		if (latitude == null) {
+			latitude = new MutableLiveData<>();
+		}
+		return latitude;
+	}
+
+	public MutableLiveData<Double> getLongitude() {
+		if (longitude == null) {
+			longitude = new MutableLiveData<>();
+		}
+		return longitude;
+	}
+
+	public MutableLiveData<Double> getAltitude() {
+		if (altitude == null) {
+			altitude = new MutableLiveData<>();
+		}
+		return altitude;
+	}
+
+	public MutableLiveData<Float> getAccuracy() {
+		if (accuracy == null) {
+			accuracy = new MutableLiveData<>();
+		}
+		return accuracy;
+	}
+
+	public MutableLiveData<Float> getSpeed() {
+		if (speed == null) {
+			speed = new MutableLiveData<>();
+		}
+		return speed;
+	}
+
+	public MutableLiveData<String> getAddress() {
+		if (address == null) {
+			address = new MutableLiveData<>();
+		}
+		return address;
+	}
+
+	public MutableLiveData<Boolean> getUpdates() {
+		if (updates == null) {
+			updates = new MutableLiveData<>();
+		}
+		return updates;
+	}
+
+	public MutableLiveData<Boolean> getSensor() {
+		if (sensor == null) {
+			sensor = new MutableLiveData<>();
+		}
+		return sensor;
+	}
+}
