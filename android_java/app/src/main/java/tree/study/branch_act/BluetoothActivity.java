f62f7f9cf88d9aa3686a916beeb8b24b014b247e
Dialogicode <104162300+dialogicode@users.noreply.github.com>
2022-07-13 10:22:12 +0900

    android_java > bt > + show bt device list in RecyclerView

--- android_java/app/src/main/java/tree/study/branch_act/BluetoothActivity.java
+++ android_java/app/src/main/java/tree/study/branch_act/BluetoothActivity.java
@@ -10,17 +10,29 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+
+import tree.study.R;
 import tree.study.base.BaseActivity;
 import tree.study.branch_vm.BluetoothViewModel;
 import tree.study.databinding.ActivityBluetoothBinding;
@@ -32,6 +44,8 @@ public class BluetoothActivity extends BaseActivity {
 	private BluetoothAdapter bt_adapter;
 	private BT_Receiver bt_receiver;
 	private ActivityResultLauncher<Intent> launcher_bt_on;
+	private ArrayList<BluetoothDevice> bt_recycler_data;
+	private BT_Recycler_Adapter bt_recycler_adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +67,12 @@ public class BluetoothActivity extends BaseActivity {
 
 		if (bt_adapter == null) not_support_bt();
 		else support_bt();
+
+		bt_recycler_data = new ArrayList<>();
+		bt_recycler_adapter = new BT_Recycler_Adapter(bt_recycler_data);
+		bind.btRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+		bind.btRecyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext(), DividerItemDecoration.VERTICAL));
+		bind.btRecyclerView.setAdapter(bt_recycler_adapter);
 	}
 
 	@Override
@@ -85,6 +105,7 @@ public class BluetoothActivity extends BaseActivity {
 		if (resultCode == RESULT_OK) {
 			Log.i(TAG, "사용자 블루투스 활성화 승인");
 			vm.getPower().setValue("OFF");
+			refresh_bt_recycler();
 		} else if (resultCode == RESULT_CANCELED) {
 			Log.i(TAG, "사용자 블루투스 활성화 거부");
 			vm.getPower().setValue("ON");
@@ -101,6 +122,9 @@ public class BluetoothActivity extends BaseActivity {
 		} else {
 			vm.getPower().setValue(bt_adapter.isEnabled() ? "OFF" : "ON");
 			vm.getFind().setValue(bt_adapter.isDiscovering() ? "STOP" : "FIND");
+
+			if (bt_adapter.isEnabled()) refresh_bt_recycler();
+			else clear_bt_recycler();
 		}
 	}
 
@@ -168,6 +192,24 @@ public class BluetoothActivity extends BaseActivity {
 		bt_adapter.cancelDiscovery();
 	}
 
+	@SuppressLint("NotifyDataSetChanged")
+	private void refresh_bt_recycler() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S &&
+		    ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+			request_permission(new String[] { Manifest.permission.BLUETOOTH_CONNECT });
+			return;
+		}
+		bt_recycler_data.clear();
+		bt_recycler_data.addAll(bt_adapter.getBondedDevices());
+		bt_recycler_adapter.notifyDataSetChanged();
+	}
+
+	@SuppressLint("NotifyDataSetChanged")
+	private void clear_bt_recycler() {
+		bt_recycler_data.clear();
+		bt_recycler_adapter.notifyDataSetChanged();
+	}
+
 	private void register_bt_receiver() {
 		if (bt_receiver == null) return;
 		IntentFilter filter = new IntentFilter();
@@ -193,6 +235,7 @@ public class BluetoothActivity extends BaseActivity {
 	private class BT_Receiver extends BroadcastReceiver {
 		private final String TAG_RCV = "DEV_BT_RCV";
 
+		@SuppressLint("NotifyDataSetChanged")
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			String action = intent.getAction();
@@ -202,8 +245,12 @@ public class BluetoothActivity extends BaseActivity {
 				BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 				int conn = intent.getIntExtra(BluetoothAdapter.EXTRA_CONNECTION_STATE, -1);
 				int conn_pre = intent.getIntExtra(BluetoothAdapter.EXTRA_PREVIOUS_CONNECTION_STATE, -1);
+
 				Log.i(TAG_RCV, "CONNECTION | addr: " + device.getAddress() + " | " +
 				               get_adapter_str(conn_pre) + " > " + get_adapter_str(conn));
+
+				if (conn == BluetoothAdapter.STATE_CONNECTED)
+					bt_recycler_adapter.notifyDataSetChanged();
 			}
 
 			// 원격 기기 검색 종료
@@ -216,6 +263,7 @@ public class BluetoothActivity extends BaseActivity {
 			else if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
 				Log.i(TAG_RCV, "DISCOVERY | STARTED");
 				vm.getFind().setValue("STOP");
+				refresh_bt_recycler();
 			}
 
 			// 로컬 기기 활성 상태 변화
@@ -225,9 +273,13 @@ public class BluetoothActivity extends BaseActivity {
 
 				Log.i(TAG_RCV, "STATE | " + get_adapter_str(power_pre) + " > " + get_adapter_str(power));
 
-				if (power == BluetoothAdapter.STATE_OFF) vm.getPower().setValue("ON");
-				else if (power == BluetoothAdapter.STATE_ON) vm.getPower().setValue("OFF");
-				else vm.getPower().setValue("~");
+				if (power == BluetoothAdapter.STATE_OFF) {
+					vm.getPower().setValue("ON");
+					clear_bt_recycler();
+				} else if (power == BluetoothAdapter.STATE_ON) {
+					vm.getPower().setValue("OFF");
+					refresh_bt_recycler();
+				} else vm.getPower().setValue("~");
 			}
 
 			// ACL 연결 생성
@@ -238,6 +290,7 @@ public class BluetoothActivity extends BaseActivity {
 			// ACL 연결 종료
 			else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
 				Log.i(TAG_RCV, "ACL | DISCONNECTED");
+				bt_recycler_adapter.notifyDataSetChanged();
 			}
 
 			// 원격 기기 페어링 상태 변화
@@ -248,6 +301,8 @@ public class BluetoothActivity extends BaseActivity {
 
 				Log.i(TAG_RCV, "BOND | addr: " + device.getAddress() + " | " +
 				               get_device_str(bond_pre) + " > " + get_device_str(bond));
+
+				bt_recycler_adapter.notifyDataSetChanged();
 			}
 
 			// 원격 기기 발견
@@ -256,6 +311,11 @@ public class BluetoothActivity extends BaseActivity {
 				BluetoothClass device_class = intent.getParcelableExtra(BluetoothDevice.EXTRA_CLASS);
 
 				Log.i(TAG_RCV, "FOUND | addr: " + device.getAddress() + " | class: " + device_class);
+
+				if (!bt_recycler_data.contains(device)) {
+					bt_recycler_data.add(device);
+					bt_recycler_adapter.notifyDataSetChanged();
+				}
 			}
 		}
 
@@ -279,6 +339,79 @@ public class BluetoothActivity extends BaseActivity {
 			if (num == BluetoothDevice.BOND_BONDED) return "BOND_BONDED";
 
 			return "NULL";
+		}
+	}
+
+	private class BT_Recycler_Adapter extends RecyclerView.Adapter<BT_Recycler_ViewHolder> {
+		private ArrayList<BluetoothDevice> data;
+
+		public BT_Recycler_Adapter(ArrayList<BluetoothDevice> data) {
+			this.data = data;
+		}
+
+		@NonNull
+		@Override
+		public BT_Recycler_ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+			View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_bluetooth, parent, false);
+			return new BT_Recycler_ViewHolder(view);
+		}
+
+		@SuppressLint("MissingPermission")
+		@Override
+		public void onBindViewHolder(@NonNull BT_Recycler_ViewHolder holder, int position) {
+			BluetoothDevice device = data.get(position);
+
+			holder.bt_name.setText(device.getName());
+			holder.bt_addr.setText(device.getAddress());
+
+			if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+				holder.bt_bond.setText("페어링 기기");
+				holder.bt_bond.setTextColor(Color.parseColor("#FF00E676"));
+			} else if (device.getBondState() == BluetoothDevice.BOND_BONDING) {
+				holder.bt_bond.setText("페어링 동작중");
+				holder.bt_bond.setTextColor(Color.parseColor("#FFFFFF00"));
+			} else if (device.getBondState() == BluetoothDevice.BOND_NONE) {
+				holder.bt_bond.setText("페어링 대기");
+				holder.bt_bond.setTextColor(Color.parseColor("#80000000"));
+			}
+
+			if (isConnected(device)) {
+				holder.bt_conn.setText("연결 기기");
+				holder.bt_conn.setTextColor(Color.parseColor("#FF00E676"));
+			} else {
+				holder.bt_conn.setText("연결 대기");
+				holder.bt_conn.setTextColor(Color.parseColor("#80000000"));
+			}
+		}
+
+		@Override
+		public int getItemCount() {
+			return data.size();
+		}
+
+		private boolean isConnected(BluetoothDevice device) {
+			try {
+				Method m = device.getClass().getMethod("isConnected", (Class[]) null);
+				boolean connected = (boolean) m.invoke(device, (Object[]) null);
+				return connected;
+			} catch (Exception e) {
+				throw new IllegalStateException(e);
+			}
+		}
+	}
+
+	private class BT_Recycler_ViewHolder extends RecyclerView.ViewHolder {
+		private final TextView bt_name;
+		private final TextView bt_addr;
+		private final TextView bt_bond;
+		private final TextView bt_conn;
+
+		public BT_Recycler_ViewHolder(@NonNull View itemView) {
+			super(itemView);
+			this.bt_name = itemView.findViewById(R.id.bt_name);
+			this.bt_addr = itemView.findViewById(R.id.bt_addr);
+			this.bt_bond = itemView.findViewById(R.id.bt_bond);
+			this.bt_conn = itemView.findViewById(R.id.bt_conn);
 		}
 	}
 }
